89ca2d1adcf099a46ca0b43184dda29bf7a58ae5
Noorullah Omarzai Wardak <noorullahomerzai123@gmail.com>
2025-08-27 18:28:17 +0430

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; 
-
 const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
